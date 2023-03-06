514dc3ac1cc14b9359e9f8b74e8a38882801dcc8
dcboules <dcboules@gmail.com>
2023-03-06 10:02:18 +0000

    fixed app.py api

--- gpio-control/app.py
+++ gpio-control/app.py
@@ -1,27 +1,55 @@
-from flask import Flask, request
+from flask import Flask
 import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+
+# Define the GPIO pins that the lights are connected to
+LIGHT_PINS = [17, 18, 19, 20]
+
+# Set up the GPIO pins for output
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-led_pins = [18, 23, 24, 25]
-GPIO.setup(led_pins, GPIO.OUT)
+GPIO.setup(LIGHT_PINS, GPIO.OUT)
 
-@app.route('/light/<int:led_number>/<action>')
-def control_led(led_number, action):
-    if led_number < 1 or led_number > len(led_pins):
-        return f"Invalid LED number. Must be between 1 and {len(led_pins)}"
+@app.route('/light1/on')
+def light1_on():
+    GPIO.output(LIGHT_PINS[0], GPIO.HIGH)
+    return 'Light 1 turned on'
 
-    if action not in ['on', 'off']:
-        return "Invalid action. Must be 'on' or 'off'"
+@app.route('/light1/off')
+def light1_off():
+    GPIO.output(LIGHT_PINS[0], GPIO.LOW)
+    return 'Light 1 turned off'
 
-    led_pin = led_pins[led_number - 1]
-    is_on = GPIO.input(led_pin)
+@app.route('/light2/on')
+def light2_on():
+    GPIO.output(LIGHT_PINS[1], GPIO.HIGH)
+    return 'Light 2 turned on'
 
-    if (action == 'on' and not is_on) or (action == 'off' and is_on):
-        GPIO.output(led_pin, not is_on)
+@app.route('/light2/off')
+def light2_off():
+    GPIO.output(LIGHT_PINS[1], GPIO.LOW)
+    return 'Light 2 turned off'
 
-    return f"LED {led_number} turned {action}"
+@app.route('/light3/on')
+def light3_on():
+    GPIO.output(LIGHT_PINS[2], GPIO.HIGH)
+    return 'Light 3 turned on'
+
+@app.route('/light3/off')
+def light3_off():
+    GPIO.output(LIGHT_PINS[2], GPIO.LOW)
+    return 'Light 3 turned off'
+
+@app.route('/light4/on')
+def light4_on():
+    GPIO.output(LIGHT_PINS[3], GPIO.HIGH)
+    return 'Light 4 turned on'
+
+@app.route('/light4/off')
+def light4_off():
+    GPIO.output(LIGHT_PINS[3], GPIO.LOW)
+    return 'Light 4 turned off'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
